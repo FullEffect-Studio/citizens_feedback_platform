43b781db04ce3c461707115040671022f177bf9b
Ben <danquahwhite@gmail.com>
2022-12-31 17:03:15 +0000

    implemented response handling

--- app/responses.py
+++ app/responses.py
@@ -1,8 +1,53 @@
+class ResponseTypes:
+    """
+    The error types contained in the ResponseTypes class are very similar to HTTP errors. This similarity will be
+    useful when we return responses from the web framework.
+
+    PARAMETERS_ERROR signals that something was wrong in the input parameters passed by the request.
+
+    RESOURCE_ERROR signals that the process ended correctly, but the requested resource isn’t available. Lastly,
+
+    SYSTEM_ERROR signals that something went wrong with the process itself. These are used mostly to signal an
+    exception in the Python code.
+    """
+    PARAMETERS_ERROR = 'ParametersError'
+    RESOURCE_ERROR = 'ResourceError'
+    SYSTEM_ERROR = 'SystemError',
+    SUCCESS = 'Success'
+
+
+class ResponseFailure:
+    def __init__(self, type_, message):
+        self.type = type_
+        self.message = self._format_message(message)
+
+    def _format_message(self, msg):
+        if isinstance(msg, Exception):
+            return "{}: {}".format(msg.__class__.__name__, "{}".format(msg))
+        return msg
+
+    @property
+    def value(self):
+        return {"type": self.type, "message": self.message}
+
+    def __bool__(self):
+        return False
+
+
 class ResponseSuccess:
     def __init__(self, value=None):
+        self.type = ResponseTypes.SUCCESS
         self.value = value
 
     def __bool__(self):
         return True
 
 
+def build_response_from_invalid_request(invalid_request):
+    message = "\n".join(
+        [
+            "{}: {}".format(err["parameter"], err["message"])
+            for err in invalid_request.errors
+        ]
+    )
+    return ResponseFailure(ResponseTypes.PARAMETERS_ERROR, message)
--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,6 +1,65 @@
-from app.responses import ResponseSuccess
+from app.requests.user_list import UserListInvalidRequest
+from app.responses import ResponseSuccess, build_response_from_invalid_request, ResponseTypes, ResponseFailure
+
+SUCCESS_VALUE = {"key": ["value1", "value2"]}
+GENERIC_RESPONSE_TYPE = "Response"
+GENERIC_RESPONSE_MESSAGE = 'This is a response'
 
 
 def test_response_success_is_true():
-    assert bool(ResponseSuccess()) is True
+    response = ResponseSuccess(SUCCESS_VALUE)
+    assert bool(response) is True
 
+
+def test_response_failure_is_false():
+    response = ResponseFailure(GENERIC_RESPONSE_TYPE, GENERIC_RESPONSE_MESSAGE)
+    assert bool(response) is False
+
+
+def test_response_success_has_type_and_value():
+    response = ResponseSuccess(SUCCESS_VALUE)
+
+    assert response.type == ResponseTypes.SUCCESS
+    assert response.value == SUCCESS_VALUE
+
+
+def test_response_failure_has_type_and_mesage():
+    response = ResponseFailure(GENERIC_RESPONSE_TYPE, GENERIC_RESPONSE_MESSAGE)
+
+    assert response.type == GENERIC_RESPONSE_TYPE
+    assert response.value == {
+        "type": GENERIC_RESPONSE_TYPE,
+        "message": GENERIC_RESPONSE_MESSAGE
+    }
+
+
+def test_response_failure_initialization_with_exception():
+    response = ResponseFailure(
+        GENERIC_RESPONSE_TYPE,
+        Exception('Just an error message')
+    )
+
+    assert bool(response) is False
+    assert response.type == GENERIC_RESPONSE_TYPE
+    assert response.message == 'Exception: Just an error message'
+
+
+def test_response_failure_from_empty_invalid_request():
+    response = build_response_from_invalid_request(
+        UserListInvalidRequest()
+    )
+
+    assert bool(response) is False
+    assert response.type == ResponseTypes.PARAMETERS_ERROR
+
+
+def test_response_failure_from_invalid_request_with_errors():
+    request = UserListInvalidRequest()
+    request.add_error('path', 'is mandatory')
+    request.add_error('path', 'cannot be blank')
+
+    response = build_response_from_invalid_request(request)
+
+    assert bool(response) is False
+    assert response.type == ResponseTypes.PARAMETERS_ERROR
+    assert response.message == "path: is mandatory\npath: cannot be blank"
