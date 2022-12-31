dd40e7791cf9c3e8cdc54f59f91d6f6c675aac84
Ben <danquahwhite@gmail.com>
2022-12-31 17:21:38 +0000

    refactored usecase to use new request and response objects

--- app/usecases/user_list.py
+++ app/usecases/user_list.py
@@ -1,7 +1,15 @@
 from app.requests.user_list import UserListValidRequest, UserListInvalidRequest
-from app.responses import ResponseSuccess
+from app.responses import ResponseSuccess, build_response_from_invalid_request, ResponseTypes, ResponseFailure
 
 
 def user_list_usecase(repo, request: UserListValidRequest | UserListInvalidRequest):
-    # Introduce error checking for invariants
-    return ResponseSuccess(repo.list())
+
+    if not request:
+        return build_response_from_invalid_request(request)
+
+    try:
+        users = repo.list(filters=request.filters)
+        return ResponseSuccess(users)
+    except Exception as e:
+        return ResponseFailure(ResponseTypes.SYSTEM_ERROR, e)
+
--- tests/usecases/test_users_list.py
+++ tests/usecases/test_users_list.py
@@ -4,7 +4,8 @@ from unittest.mock import MagicMock, Mock
 import pytest
 
 from app.domain.user import User, UserRole
-from app.requests.user_list import  build_user_list_request
+from app.requests.user_list import build_user_list_request
+from app.responses import ResponseTypes
 from app.usecases.user_list import user_list_usecase
 
 
@@ -51,6 +52,48 @@ def test_room_list_without_parameters(domain_users):
 
     assert bool(response) is True
 
-    repo.list.assert_called_with()
+    repo.list.assert_called_with(filters=None)
 
     assert response.value == domain_users
+
+def test_user_list_with_filters(domain_users):
+    repo = Mock()
+    repo.list.return_value = domain_users
+
+    qry_filters = {"role__eq": "Administrator"}
+    request = build_user_list_request(filters=qry_filters)
+
+    response = user_list_usecase(repo, request)
+
+    assert bool(response) is True
+    repo.list.assert_called_with(filters=qry_filters)
+    assert response.value == domain_users
+
+
+def test_user_list_handles_generic_error():
+    repo = Mock()
+    repo.list.side_effect = Exception('Just an error message')
+
+    request = build_user_list_request(filters={})
+
+    response = user_list_usecase(repo, request)
+
+    assert bool(response) is False
+
+    assert response.value == {
+        "type": ResponseTypes.SYSTEM_ERROR,
+        "message": "Exception: Just an error message"
+    }
+
+def test_room_list_handles_bad_request():
+    repo = Mock()
+
+    request = build_user_list_request(filters=5)
+
+    response = user_list_usecase(repo, request)
+
+    assert bool(response) is False
+    assert response.value == {
+        "type": ResponseTypes.PARAMETERS_ERROR,
+        "message": "filters: Is not iterable",
+    }
