951aceca0f0dcc531ddb4d90f3940ce8e42baeb8
Ben <danquahwhite@gmail.com>
2022-12-31 10:56:16 +0000

    implemented an in memory data repository

---
+++ tests/repository/test_memrepo.py
@@ -0,0 +1,39 @@
+import pytest
+
+from app.domain.user import User
+from app.repository.memrepo import MemRepo
+
+
+@pytest.fixture
+def users_dicts():
+    return [
+        {
+            "id": "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+            "username": "Danquah",
+            "password": "pass111"
+        },
+        {
+            "id": "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
+            "username": "Fahrid",
+            "password": "pass222"
+        },
+        {
+            "id": "913694c6-435a-4366-ba0d-da5334a611b2",
+            "username": "Yau",
+            "password": "pass333"
+        },
+
+        {
+            "id": "913694c6-435a-4366-ba0d-da5334a611b2",
+            "username": "Lowe",
+            "password": "pass444"
+        },
+    ]
+
+
+def test_repository_list_users_without_parameters(users_dicts):
+    repo = MemRepo(users_dicts)
+
+    users = [User.from_dict(i) for i in users_dicts]
+
+    assert repo.list() == users
