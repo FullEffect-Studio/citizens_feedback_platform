e33072fd30f89f3aaaa064c4b134aa420fc41ef5
Ben <danquahwhite@gmail.com>
2022-12-31 13:55:03 +0000

    refactord wsgi executor

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,12 @@ import os
 
 from web.app import create_app
 
-env = os.environ['FLASK_CONFIG']
-print(f'Executing app in {env} environment')
-app = create_app(os.environ['FLASK_CONFIG'])
+try:
+    env = os.environ['FLASK_CONFIG']
+    print(f'Executing app in {env} environment')
+except KeyError:
+    print('FLASK_CONFIG env variable was not found.')
+    print('Defaulting to development environment')
+    env = 'development'
+
+app = create_app(env)
