ef8a87b7acc34063042b84183c6e81ed593ef79e
Ben <danquahwhite@gmail.com>
2023-01-01 03:51:34 +0000

    updated usecase tests

--- tests/usecases/test_users_list.py
+++ tests/usecases/test_users_list.py
@@ -16,26 +16,25 @@ def dto_user_list():
         role=UserRole.COMMUNITY_SOCIAL_WORKER
     )
 
-    # user_2 = UserListDto(
-    #     id=uuid4(),
-    #     username="Fahrid",
-    #     role=UserRole.PUBLIC_OFFICIAL
-    # )
-    #
-    # user_3 = UserListDto(
-    #     id=uuid4(),
-    #     username="Yau",
-    #     role=UserRole.PUBLIC_OFFICIAL
-    # )
-    #
-    # user_4 = UserListDto(
-    #     id=uuid4(),
-    #     username="Lowe",
-    #     role=UserRole.ADMIN
-    # )
+    user_2 = UserListDto(
+        id="fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
+        username="Fahrid",
+        role=UserRole.PUBLIC_OFFICIAL
+    )
 
-    return [user_1]
-            # user_2, user_3, user_4]
+    user_3 = UserListDto(
+        id="913694c6-435a-4366-ba0d-da5334a611b2",
+        username="Yau",
+        role=UserRole.PUBLIC_OFFICIAL
+    )
+
+    user_4 = UserListDto(
+        id="913694c6-435a-4366-ba0d-da5334a611b2",
+        username="Lowe",
+        role=UserRole.ADMIN
+    )
+
+    return [user_1, user_2, user_3, user_4]
 
 
 def test_room_list_without_parameters(dto_user_list):
