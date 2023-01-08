d7c42cb6d90d44977f7cd5293534f90ac0ded6e3
Ben <danquahwhite@gmail.com>
2023-01-08 18:02:45 +0000

    updates

--- backend/cfp/wsgi.py
+++ backend/cfp/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-from web.app import create_app
+from cfp.web.app import create_app
 
 try:
     env = os.environ['FLASK_CONFIG']
@@ -8,7 +8,6 @@ try:
 except KeyError:
     # print('FLASK_CONFIG env variable was not found.')
     # print('Defaulting to development environment')
-
     env = 'development'
 
 app = create_app(env)
---
+++ frontend/pages/Users.js
@@ -0,0 +1,6 @@
+export default function Feedback(){
+
+    return (
+        <h1>Upload Feedback</h1>
+    )
+}
